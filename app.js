8023591dda154e02219ad2a728605dea58acf15f
Hong8076 <anhong8076@hotmail.com>
2026-06-29 15:24:32 -0400

    Add functions to open various pages with alerts

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log("Collection & Review Started!");
+function openCollection(){
+
+alert("Collection 页面（开发中）");
+
+}
+
+function openReview(){
+
+alert("Review 页面（开发中）");
+
+}
+
+function openKnowledge(){
+
+alert("Knowledge Base 页面（开发中）");
+
+}
+
+function openRewards(){
+
+alert("Rewards 页面（开发中）");
+
+}
